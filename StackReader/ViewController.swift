361a7997898ff89ec16abde3fc0affe9c556a61a
Pat Trudel <patrick.trudel@me.com>
2021-05-30 08:54:04 -0400

    Initial Commit

---
+++ StackReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackReader
+//
+//  Created by Patrick Trudel on 2021-05-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
