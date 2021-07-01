296c38cf9c12af3651e235c9b8403dcc85a52633
Pat Trudel <patrick.trudel@me.com>
2021-07-01 12:31:20 -0400

    1.2 (2)

--- StackReader/Extension+UICollectionViewLayout.swift
+++ StackReader/Extension+UICollectionViewLayout.swift
@@ -56,6 +56,60 @@ extension UICollectionViewLayout {
         return layout
     }
     
+    static var hierarchicalLayout: UICollectionViewCompositionalLayout {
+        let itemInset = NSDirectionalEdgeInsets(top: 4.0, leading: 4.0, bottom: 4.0, trailing: 4.0)
+        let sectionInset = NSDirectionalEdgeInsets(top: 4.0, leading: 16.0, bottom: 20.0, trailing: 16.0)
+        let isIpad = UIDevice.current.userInterfaceIdiom == .pad
+        let isHorizontal = UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, environment in
+            // Large Item
+            let largeItem = NSCollectionLayoutItem(
+                layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.0))
+            )
+            largeItem.contentInsets = itemInset
+            
+            // Small Item
+            let smallItem = NSCollectionLayoutItem(
+                layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalWidth(0.5))
+            )
+            smallItem.contentInsets = itemInset
+            
+            // Horizontal Group
+            let hGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.25)),
+                subitems: [smallItem, smallItem]
+            )
+            
+            // Vertical Group
+            let vGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(1.5)),
+                subitems: [
+                    largeItem,
+                    hGroup
+                ]
+            )
+            
+            // Section
+            let section = NSCollectionLayoutSection(group: vGroup)
+            section.contentInsets = sectionInset
+            
+            let footerItem = NSCollectionLayoutBoundarySupplementaryItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.9 * (isIpad ? 0.75 : 1.0) * (isHorizontal ? 0.75 : 1.0)),
+                    heightDimension: .estimated(500)
+                ),
+                elementKind: UICollectionView.elementKindSectionFooter,
+                alignment: .bottom
+            )
+            let showAd = sectionIndex % AdManager.adFrequency == 0 &&
+                sectionIndex != 0 &&
+                !AdManager.shared.adQueueIsEmpty
+            section.boundarySupplementaryItems = showAd ? [footerItem] : []
+            return section
+        }
+        return layout
+    }
+    
     static var stacksLayout: UICollectionViewCompositionalLayout {
         let itemInset = NSDirectionalEdgeInsets(top: 2.0, leading: 0.0, bottom: 2.0, trailing: 16.0)
         let sectionInset = NSDirectionalEdgeInsets(top: 4.0, leading: 16.0, bottom: 20.0, trailing: 8.0)
